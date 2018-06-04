5d491855dab9969377ab4497d82635db523a2c37
El Salvador <00125016@uca.edu.sv>
2018-06-04 15:44:21 -0600

    Cambia de fragment segun se seleccione en el navigation drawer

--- app/src/main/java/com/example/elsalvador/parcial/Activities/Main2Activity.java
+++ app/src/main/java/com/example/elsalvador/parcial/Activities/Main2Activity.java
@@ -1,9 +1,7 @@
-package com.example.elsalvador.parcial;
+package com.example.elsalvador.parcial.Activities;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,8 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.elsalvador.parcial.Fragment.CSGO;
+import com.example.elsalvador.parcial.Fragment.Dota;
+import com.example.elsalvador.parcial.Fragment.LOL;
+import com.example.elsalvador.parcial.Fragment.News;
+import com.example.elsalvador.parcial.R;
+import com.example.elsalvador.parcial.Fragment.Set;
+import com.example.elsalvador.parcial.Fragment.fav;
+
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Fragment changefragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +31,6 @@ public class Main2Activity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -39,7 +39,7 @@ public class Main2Activity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -81,23 +81,31 @@ public class Main2Activity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.News) {
-            // Handle the camera action
-        } else if (id == R.id.News) {
-
-        } else if (id == R.id.LOL) {
-
+            replaceFragment(new News());
+        }  else if (id == R.id.LOL) {
+            replaceFragment(new LOL());
         } else if (id == R.id.Dota) {
-
+            replaceFragment(new Dota());
         } else if (id == R.id.CSGO) {
-
+            replaceFragment(new CSGO());
         } else if (id == R.id.Set) {
-
+            replaceFragment(new Set());
         }else if (id == R.id.Fav) {
-
+            replaceFragment(new fav());
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+
     }
+
+    private void replaceFragment(Fragment fragment) {
+        if (changefragment == null || !fragment.getClass().getName().equals(changefragment.getClass().getName())) {
+            getFragmentManager().beginTransaction().replace(R.id.replacefragment, fragment).commit();
+            changefragment = fragment;
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/elsalvador/parcial/Fragment/fav.java
@@ -0,0 +1,32 @@
+package com.example.elsalvador.parcial.Fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.elsalvador.parcial.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fav extends Fragment {
+
+
+    public fav() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fav, container, false);
+    }
+
+}
